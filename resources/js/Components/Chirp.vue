6d6cb1050112025d67123b22a1225f00cc6fe446
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-10 14:07:01 +0200

    allowed deleting chirps

--- resources/js/Components/Chirp.vue
+++ resources/js/Components/Chirp.vue
@@ -2,6 +2,7 @@
 import Dropdown from '@/Components/Dropdown.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { useForm } from '@inertiajs/vue3';
@@ -28,23 +29,26 @@ const editing = ref(false);
                 <div>
                     <span class="text-gray-800">{{ chirp.user.name }}</span>
                     <small class="ml-2 text-sm text-gray-600">{{ dayjs(chirp.created_at).fromNow() }}</small>
-                    <small v-if="chirp.creatd_at !== chirp.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
+                    <small v-if="chirp.created_at !== chirp.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
                 </div>
+                <Dropdown v-if="chirp.user.id === $page.props.auth.user.id">
+                    <template #trigger>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </template>
+                    <template #content>
+                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
+                            Edit
+                        </button>
+                        <DropdownLink as="button" :href="route('chirps.destroy', chirp.id)" method="delete">
+                            Delete
+                        </DropdownLink>
+                    </template>
+                </Dropdown>
             </div>
-        <Dropdown v-if="chirp.user.id === $page.props.auth.user.id">
-            <template #trigger>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                    </svg>
-                </button>
-            </template>
-            <template #content>
-                <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
-                    Edit
-                </button>
-            </template>
-        </Dropdown>
             <form v-if="editing" @submit.prevent="form.put(route('chirps.update', chirp.id), { onSuccess: () => editing = false })">
                 <textarea v-model="form.message" class="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
                 <InputError :message="form.errors.message" class="mt-2" />
